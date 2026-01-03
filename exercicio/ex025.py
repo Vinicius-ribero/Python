14e99adaaf19f4d8cc4d40de06ff29310a6ad1ff
Vinicius-ribero <viniciusribeirojesus77@gmail.com>
2026-01-03 19:23:36 -0300

    correção exercícios

--- exercicio/ex025.py
+++ exercicio/ex025.py
@@ -1,9 +1,16 @@
 import random
-
-computador = random.randint(0,100)
-
-chute = int(input("Adivinha o núemro de 0 a 100: "))
-
-if chute == computador:
-    print("Você Venceu!")
-else: (f"Você perdeu! O computador pensou no {computador} ")
+from random import randint
+from time import sleep
+#FAZ COMPUTADOR PENSAR
+computador = randint (0,5) 
+print("-=-" *20)
+print("Vou pensar em um número entre 0 e 5. Tente adivinhar")
+print("-=-"*20)
+#JOGADOR TENTA ADIVINHAR
+jogador = int(input("Em que número eu pensei?"))
+print("PROCESSANDO....")
+sleep(3)
+if jogador == computador:
+    print("Parabéns!Você consegui me vencer!")
+else: 
+    print("Você perdeu! tente novante ")
--- exercicio/ex029.py
+++ exercicio/ex029.py
@@ -20,3 +20,15 @@ if ano % 4 == 0:
         print("É Bissexto")
 else:
     print("Não é Bissexto")
+
+
+#AVANÇADO
+
+from datetime import date
+ano = int(input("Que ano quer analisr? Coloque 0 para analisar o ano atual: "))
+if ano == 0:
+    ano = date.today(). year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print(f"O ano {ano} é BISSEXTO")
+else:
+    print(f"O ano {ano} não é BISSEXTO")
